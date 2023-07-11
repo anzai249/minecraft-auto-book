d11199fb24d91e3ab10f0320e1f32b9e15eceaa7
an <a381811169@163.com>
2023-07-11 20:12:37 +0800

    readme and auto exit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pyperclip
 import unicodedata
 import time
-import keyboard
+from pynput import keyboard
+import os
 
 def Is_CJK(char):
     # print(unicodedata.name(char))
@@ -26,6 +27,16 @@ try:
 except FileNotFoundError:
     print(f'{file_name} Not Found!')
 
+control = keyboard.Controller()
+
+print(3)
+time.sleep(1)
+print(2)
+time.sleep(1)
+print(1)
+time.sleep(1)
+print('Begin!')
+
 page = ''
 page_top = 255
 now_index = 0
@@ -34,20 +45,25 @@ i = 0
 while True:
     i = 0
     while i < page_top:
-        if(ord(file_content[now_index]) != 10):
-            if Is_CJK(file_content[now_index]):
-                i += 1
+        try:
+            if(ord(file_content[now_index]) != 10):
+                if Is_CJK(file_content[now_index]):
+                    i += 1
+        except IndexError:
+            print('May be your text is too short or the text is ending, please copy it manually.')
+            os._exit(1)
         page = page + file_content[now_index]
         now_index += 1
         i += 1
     time.sleep(1.5)
     pyperclip.copy(page)
-    keyboard.press_and_release('ctrl+v')
+    
+    with control.pressed(keyboard.Key.ctrl):
+        control.press('v')
+        control.release('v')
     time.sleep(1.5)
-    keyboard.press_and_release('PgDn')
-    # pyperclip.paste()
-    print(page)
+    control.press(keyboard.Key.page_down)
+    control.release(keyboard.Key.page_down)
+    time.sleep(0.5)
+    # print(page)
     page = ''
-
-
-# print(file_content)
